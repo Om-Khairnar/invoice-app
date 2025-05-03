21d3a46dbaac111f90a64df28aaf6517cbdb0c80
Om Khairnar <om.khairnar175@gmail.com>
2025-05-04 02:05:54 +0530

    adding the fresh file

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { requiredUser } from "../utils/hooks";
+import { signOut } from "../utils/auth";
+export default async function DashboardRoute() {
+  const session = await requiredUser();
+  return (
+    <>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
+    </>
+  );
+}
---
+++ app/utils/zodSchemas.ts
@@ -0,0 +1,29 @@
+import { totalmem } from "os";
+import { z } from "zod";
+
+export const onboardingSchema = z.object({
+  firstName: z.string().min(2, "First name is required"),
+  lastName: z.string().min(2, "Last name is required"),
+  address: z.string().min(2, "Address is required"),
+});
+
+export const invoiceSchema = z.object({
+    invoiceName: z.string().min(1, "Inoice name is required"),
+    total: z.string().min(1, "1 $ is minimum"),
+    status: z.enum(["PAID", "PENDING"]).default("PENDING"),
+    date: z.string().min(1, "Due date is required"),
+    dueDate: z.number().min(1, "Due date is required"),
+    fromName: z.string().min(1, "Your name is required"),
+    fromEmail: z.string().email("Email is required"),
+    fromAddress: z.string().min(1, "Address is required"),
+    clientName: z.string().min(1, "Client name is required"),
+    clientEmail: z.string().email("Invalid email address"),
+    clientAddress: z.string().min(1, "Client address is required"),
+    currency: z.string().min(1, "Currency is required"),
+    invoiceNumber: z.number().min(1, " Minimum Invoice number of 1"),
+    note: z.string().optional(),
+    invoiceItemDescription: z.string().min(1, "Item description is required"),
+    invoiceItemQuantity: z.number().min(1, "quantity minimum is 1"),
+    invoiceItemRate: z.number().min(1, "Rate minimum is 1"),
+    
+})
